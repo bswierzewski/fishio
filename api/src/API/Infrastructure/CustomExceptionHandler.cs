875b73bc8c50c85597f6f4a4a80b401ec720b787
bswierzewski <swierzewski.bartosz@gmail.com>
2025-06-07 20:23:00 +0200

    Add handling for InvalidOperationException in CustomExceptionHandler
    Updated the CustomExceptionHandler to include a new method for handling InvalidOperationException. This addition sets the response status to 400 Bad Request and provides a detailed problem description in the response body, enhancing error management and user feedback for invalid operations.

--- api/src/API/Infrastructure/CustomExceptionHandler.cs
+++ api/src/API/Infrastructure/CustomExceptionHandler.cs
@@ -17,6 +17,7 @@ public class CustomExceptionHandler : IExceptionHandler
                 { typeof(NotFoundException), HandleNotFoundException },
                 { typeof(UnauthorizedAccessException), HandleUnauthorizedAccessException },
                 { typeof(ForbiddenAccessException), HandleForbiddenAccessException },
+                { typeof(InvalidOperationException), HandleInvalidOperationException },
             };
     }
 
@@ -82,6 +83,19 @@ public class CustomExceptionHandler : IExceptionHandler
             Status = StatusCodes.Status403Forbidden,
             Title = "Forbidden",
             Type = "https://tools.ietf.org/html/rfc7231#section-6.5.3"
+        });
+    }
+
+    private async Task HandleInvalidOperationException(HttpContext httpContext, Exception ex)
+    {
+        httpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+
+        await httpContext.Response.WriteAsJsonAsync(new ProblemDetails
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Title = "Bad Request",
+            Detail = ex.Message,
+            Type = "https://tools.ietf.org/html/rfc7231#section-6.5.1"
         });
     }
 }
