183981cd2ef86060ae71960ddcafbb476d9f67cb
bswierzewski <swierzewski.bartosz@gmail.com>
2025-06-15 07:04:07 +0200

    Update competition prize descriptions for clarity and conciseness
    This commit modifies the descriptions of various competition prizes in the ApplicationDbContextModelSnapshot to provide clearer and more concise information. The changes enhance the understanding of prize categories, ensuring that users have a better grasp of the competition rewards.

---
+++ api/src/Infrastructure/Persistence/Migrations/20250615050356_Update competitions category.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updatecompetitionscategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "CategoryDefinitions",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Description",
+                value: "Nagroda za największą rybę wybranego gatunku.");
+
+            migrationBuilder.UpdateData(
+                table: "CategoryDefinitions",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Description",
+                value: "Nagroda za największą rybę wybranego gatunku.");
+
+            migrationBuilder.UpdateData(
+                table: "CategoryDefinitions",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Description",
+                value: "Nagroda za największą różnorodność gatunków.");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "CategoryDefinitions",
+                keyColumn: "Id",
+                keyValue: 6,
+                column: "Description",
+                value: "Nagroda za największą rybę wybranego gatunku (np. Największy Szczupak). Gatunek wybierany przy dodawaniu kategorii do zawodów.");
+
+            migrationBuilder.UpdateData(
+                table: "CategoryDefinitions",
+                keyColumn: "Id",
+                keyValue: 7,
+                column: "Description",
+                value: "Nagroda za największą rybę wybranego gatunku (np. Największy Karp). Gatunek wybierany przy dodawaniu kategorii do zawodów.");
+
+            migrationBuilder.UpdateData(
+                table: "CategoryDefinitions",
+                keyColumn: "Id",
+                keyValue: 8,
+                column: "Description",
+                value: "Nagroda dla uczestnika, który złowił najwięcej różnych gatunków ryb.");
+        }
+    }
+}
